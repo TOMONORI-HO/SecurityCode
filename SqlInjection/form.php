134f4bb4a96764ba909979152840381b15c132d8
TOMONORI-HO <151363373+TOMONORI-HO@users.noreply.github.com>
2025-08-29 16:18:57 +0900

    Create form.php

---
+++ SqlInjection/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ユーザー検索フォーム</title>
+</head>
+<body>
+    <h2>ユーザーIDで検索</h2>
+    <form action="search.php" method="POST">
+        <label for="id">ID：</label>
+        <input type="text" name="id" id="id">
+        <button type="submit">検索</button>
+    </form>
+</body>
+</html>
