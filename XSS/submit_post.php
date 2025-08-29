29198fa0125f582c958324e9ef2d3edf90ef9b3d
TOMONORI-HO <151363373+TOMONORI-HO@users.noreply.github.com>
2025-08-29 16:16:40 +0900

    Create submit_post.php

---
+++ XSS/submit_post.php
@@ -0,0 +1,15 @@
+<?php
+$nickname = $_POST['nickname'] ?? '';
+
+// XSS対策：HTMLに出力する前にエスケープ
+$safe_nickname = htmlspecialchars($nickname, ENT_QUOTES, 'UTF-8');
+// $safe_nickname = $nickname;
+?>
+
+<!DOCTYPE html>
+<html>
+<head><meta charset="UTF-8"><title>結果</title></head>
+<body>
+    <p>こんにちは、<?php echo $safe_nickname; ?>さん！</p>
+</body>
+</html>
